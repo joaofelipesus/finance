803dd521e909a3babf51f96fa7f664c4731f5d63
joaofelipe <joaofelipe1294@gmail.com>
2025-02-23 14:14:32 -0300

    ref: style operation kind chart and form

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -7,6 +7,12 @@ class Operation < ApplicationRecord
     spend: "spend"
   }.freeze
 
+  KIND_COLORS = {
+    "spend" => "#FF0000",    # Red
+    "earning" => "#00FF00",  # Green
+    "investment" => "#0000FF" # Blue
+  }
+
   belongs_to :account
 
   validates :value, :kind, :date, presence: true
